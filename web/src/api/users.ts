dae9a25d3bd185ea375d771992b961c54ad0988b
nabilahks <nabilahksunarto@gmail.com>
2025-08-13 14:41:42 +0700

    fix: respond data from api

--- web/src/api/users.ts
+++ web/src/api/users.ts
@@ -1,8 +1,12 @@
 import type { User } from '../types';
 
 export async function getUsers(): Promise<User[]> {
-  const res = await fetch('/api/users'); // cookie httpOnly ikut otomatis (via proxy)
+  const res = await fetch('/api/users');
+
   if (res.status === 401) throw new Error('unauthorized');
   if (!res.ok) throw new Error(await res.text());
-  return res.json();
+  
+  const json = await res.json();
+
+  return json.data;
 }
